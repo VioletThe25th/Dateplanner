e7850e0e6a23bb394013fc6d4edde681d1aefad8
Jeremy Bilger <bilgerjeremy5705@gmail.com>
2026-03-19 19:56:09 +0900

    Initial Commit

---
+++ Dateplanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dateplanner
+//
+//  Created by Jeremy Bilger on 2026/03/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dateplanner/DateplannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateplannerApp.swift
+//  Dateplanner
+//
+//  Created by Jeremy Bilger on 2026/03/19.
+//
+
+import SwiftUI
+
+@main
+struct DateplannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
